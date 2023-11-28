ea2d851e661458f4aaad42a6c600d3b157885f86
azamat <azik8423@gmail.com>
2023-11-28 22:07:34 +0500

    'lesson_3_done'

---
+++ lesson_3/food/app_food/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def products_list(request, *args, **kwargs):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        # print(products)
+        context = {'products': products}
+        
+        return render(request, 'app_food/index.html',
+                      context=context)
+
+
